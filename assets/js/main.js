83fcdb8d24e662c6bd1c293d820587cf77555f38
Raxeyn <56265569+Raxeyn@users.noreply.github.com>
2024-01-04 08:38:15 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ var typewriter = new Typewriter("#main-text");
 typewriter
 	.pauseFor(500)
 	.changeDelay(75)
-	.typeString('Rezo takes care of your Discord music needs')
+	.typeString('Setaur takes care of your Discord music needs')
 	.pauseFor(2500)
 	.deleteAll()
 	.changeDelay(75)
@@ -13,7 +13,7 @@ typewriter
 	.pauseFor(2500)
 	.deleteAll()
 	.changeDelay(500)
-	.typeString("REZO")
+	.typeString("Setaur")
 	// .pauseFor(1000)
 	// .deleteAll()
 	// .typeString("REZO")
